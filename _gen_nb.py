d1e02af362d3392589d966713c33d4944a9c6f9c
Mn3TR <3397249463@qq.com>
2026-06-27 18:26:21 +0800

    fix: notebook %cd /content 先切出去再删目录，避免 shell 丢失 CWD

--- _gen_nb.py
+++ _gen_nb.py
@@ -31,9 +31,11 @@ cells.append(nbf.v4.new_markdown_cell(
 cells.append(nbf.v4.new_markdown_cell("## 1. 环境"))
 
 cells.append(nbf.v4.new_code_cell(
+    "# 回到 /content 再删（避免删掉当前目录导致 shell 挂掉）\n"
+    "%cd /content\n"
     "!rm -rf /content/a-network\n"
-    "!git clone https://github.com/Mn3TR/a-network.git /content/a-network\n"
-    "%cd /content/a-network\n"
+    "!git clone https://github.com/Mn3TR/a-network.git\n"
+    "%cd a-network\n"
     "!pip install -q torch tokenizers numpy matplotlib datasets\n"
     "import torch\n"
     "print(f'PyTorch {torch.__version__}  CUDA: {torch.cuda.is_available()}')\n"
@@ -58,7 +60,7 @@ cells.append(nbf.v4.new_code_cell(
     "for mod in list(sys.modules.keys()):\n"
     "    if mod.startswith('src.'):\n"
     "        del sys.modules[mod]\n"
-    "!rm -rf /content/a-network/src/anetwork/__pycache__\n"
+    "!rm -rf src/anetwork/__pycache__\n"
     "sys.path.insert(0, '.')\n"
     "\n"
     "from src.anetwork.config import ANetworkConfig\n"
